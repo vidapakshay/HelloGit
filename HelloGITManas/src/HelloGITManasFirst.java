5c9d2bfbb8d04ab7ea08d9c2bdfe0bb5f749b9c1
Manas Bajaj <manas.bajaj@intercax.com>
2012-05-04 14:47:38 -0400

    Added HelloGITManasSecond

--- HelloGITManas/src/HelloGITManasFirst.java
+++ HelloGITManas/src/HelloGITManasFirst.java
@@ -4,5 +4,12 @@ public class HelloGITManasFirst {
 	public static void main(String args[])
 	{
 		System.out.println("Hello GIT Manas First");
+		
 	}
+	
+	public static void check()
+	{
+		System.out.println("Check method");
+	}
+	
 }
